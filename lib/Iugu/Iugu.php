22a323880c38d8471dabd7a3d375055a7eca7016
Lucas Assis <eng_assys@MacBook-Pro-de-Lucas.local>
2020-01-03 11:38:12 -0300

    separate Iugu and Iugu Resources abstract class from Previous Base Class

---
+++ lib/Iugu/Iugu.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Iugu;
+
+abstract class Iugu
+{
+  const VERSION = '1.0.6';
+
+  public static $api_key = null;
+  public static $api_version = 'v1';
+  public static $endpoint = 'https://api.iugu.com';
+
+  public static function getBaseURI()
+  {
+    return self::$endpoint . '/' . self::$api_version;
+  }
+
+  public static function setApiKey($_api_key)
+  {
+    self::$api_key = $_api_key;
+  }
+
+  public static function getApiKey()
+  {
+    return self::$api_key;
+  }
+}
